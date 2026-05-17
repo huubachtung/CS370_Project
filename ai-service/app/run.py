06327e317de3c01f715b282a9f834867ea2cafa7
ndtribk <ndtribk@gmail.com>
2026-05-17 14:16:10 +0700

    update

--- ai-service/app/run.py
+++ ai-service/app/run.py
@@ -3,10 +3,10 @@ import uvicorn
 def main(): 
     uvicorn.run(
         "app.main:app", 
-        host = "0.0.0.0", 
-        port = 8000 , 
-        reload = True, 
-        log_level= "info"
+        host="0.0.0.0", 
+        port=8000, 
+        reload=True, 
+        log_level="info"
     )
 
 if __name__ == "__main__": 
